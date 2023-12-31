51b7765919b4e028db946b83f78b02391ffa0825
oyyd <oyydoibh@gmail.com>
2023-12-31 15:31:03 +0800

    feat: add basic apis

--- bindings/src/error.rs
+++ bindings/src/error.rs
@@ -1,40 +1,30 @@
-use kcp;
 use thiserror;
 
-impl From<std::net::AddrParseError> for SwiftKcpError {
-  fn from(value: std::net::AddrParseError) -> Self {
-    SwiftKcpError::Default {
-      msg: value.to_string(),
+macro_rules! impl_from {
+  ($from_type:ty) => {
+    impl From<$from_type> for SwiftKcpError {
+      fn from(value: $from_type) -> Self {
+        SwiftKcpError::Default {
+          msg: value.to_string(),
+        }
+      }
     }
-  }
+  };
 }
 
-impl From<kcp::Error> for SwiftKcpError {
-  fn from(value: kcp::Error) -> Self {
-    SwiftKcpError::Default {
-      msg: value.to_string(),
-    }
-  }
-}
-
-impl From<std::io::Error> for SwiftKcpError {
-  fn from(value: std::io::Error) -> Self {
-    SwiftKcpError::Default {
-      msg: value.to_string(),
-    }
-  }
-}
-
-impl From<tokio::task::JoinError> for SwiftKcpError {
-  fn from(value: tokio::task::JoinError) -> Self {
-    SwiftKcpError::Default {
-      msg: value.to_string(),
-    }
-  }
-}
+impl_from!(std::net::AddrParseError);
+impl_from!(kcp::Error);
+impl_from!(std::io::Error);
+impl_from!(tokio::task::JoinError);
 
 #[derive(Debug, thiserror::Error, uniffi::Error)]
 pub enum SwiftKcpError {
   #[error("{msg}")]
   Default { msg: String },
+
+  #[error("RUNTIME not inited")]
+  RuntimeNotInited,
+
+  #[error("Stream not found for id {id}")]
+  NoStreamForId { id: u64 },
 }
--- bindings/src/lib.rs
+++ bindings/src/lib.rs
@@ -1,12 +1,14 @@
 uniffi::include_scaffolding!("bindings");
 
 mod error;
+mod kcp_util;
 mod manager;
 
 use error::SwiftKcpError;
+pub use kcp_util::KcpConfigParams;
 use lazy_static::lazy_static;
 use manager::{StreamId, StreamManager};
-use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4};
+use std::net::SocketAddr;
 use std::str::FromStr;
 use std::sync::Arc;
 use tokio::{
@@ -18,6 +20,8 @@ use tokio_kcp::{KcpConfig, KcpStream};
 
 type Result<T> = std::result::Result<T, error::SwiftKcpError>;
 
+const READ_BUF: usize = 65535;
+
 lazy_static! {
   static ref RUNTIME: Arc<Mutex<Option<Runtime>>> = Arc::new(Mutex::new(None));
   static ref MANAGER: Arc<StreamManager> = Arc::new(StreamManager::new());
@@ -25,11 +29,8 @@ lazy_static! {
 
 #[uniffi::export]
 async fn init_runtime() -> Result<()> {
-  // TODO support parameters for runtimes
   let rt = Runtime::new()?;
-
   let mut runtime = RUNTIME.lock().await;
-
   let _ = runtime.insert(rt);
 
   Ok(())
@@ -37,29 +38,38 @@ async fn init_runtime() -> Result<()> {
 
 #[uniffi::export]
 async fn deinit_runtime() -> Result<()> {
-  let mut runtime = RUNTIME.lock().await;
+  let rt = {
+    let mut runtime = RUNTIME.lock().await;
 
-  runtime.take();
+    runtime.take()
+  };
+
+  if rt.is_some() {
+    rt.unwrap()
+      .shutdown_timeout(std::time::Duration::from_secs(1));
+  }
 
   Ok(())
 }
 
 #[uniffi::export]
-async fn new_stream(addr_str: String) -> Result<StreamId> {
-  let config = KcpConfig::default();
+fn default_kcp_config_params() -> KcpConfigParams {
+  KcpConfigParams::default()
+}
+
+#[uniffi::export]
+async fn new_stream(addr_str: String, params: KcpConfigParams) -> Result<StreamId> {
+  let config: KcpConfig = params.into();
   let addr = SocketAddr::from_str(&addr_str)?;
 
   let join_handle = {
     let mut rt = RUNTIME.lock().await;
     if rt.is_none() {
-      return Err(SwiftKcpError::Default {
-        msg: "RUNTIME not inited".to_string(),
-      });
+      return Err(SwiftKcpError::RuntimeNotInited);
     }
     let rt = rt.as_mut().unwrap();
     rt.spawn(async move {
       let stream = KcpStream::connect(&config, addr).await;
-      // stream
       stream
     })
   };
@@ -76,9 +86,7 @@ async fn remove_stream(id: StreamId) -> Result<()> {
   let stream = MANAGER.remove_stream(id);
 
   if stream.is_none() {
-    return Err(SwiftKcpError::Default {
-      msg: format!("stream with id: {} doesn't exit", id),
-    });
+    return Err(SwiftKcpError::NoStreamForId { id });
   }
 
   Ok(())
@@ -87,23 +95,17 @@ async fn remove_stream(id: StreamId) -> Result<()> {
 #[uniffi::export]
 async fn write_stream(id: StreamId, data: Vec<u8>) -> Result<()> {
   let mut rt = RUNTIME.lock().await;
-
   if rt.is_none() {
-    return Err(SwiftKcpError::Default {
-      msg: "RUNTIME not inited".to_string(),
-    });
+    return Err(SwiftKcpError::RuntimeNotInited);
   }
-
   let rt = rt.as_mut().unwrap();
 
   rt.spawn(async move {
     let stream = MANAGER.get_mut_stream(id);
     if stream.is_none() {
-      return Err(SwiftKcpError::Default {
-        msg: format!("no stream with id {}", id),
-      });
+      return Err(SwiftKcpError::NoStreamForId { id });
     }
-    let mut stream = stream.unwrap();
+    let mut stream: dashmap::mapref::one::RefMut<'_, u64, KcpStream> = stream.unwrap();
     stream.write_all(&data).await?;
 
     Ok(())
@@ -115,5 +117,24 @@ async fn write_stream(id: StreamId, data: Vec<u8>) -> Result<()> {
 
 #[uniffi::export]
 async fn read_stream(id: StreamId) -> Result<Vec<u8>> {
-  Ok(vec![])
+  let mut rt = RUNTIME.lock().await;
+  if rt.is_none() {
+    return Err(SwiftKcpError::RuntimeNotInited);
+  }
+  let rt = rt.as_mut().unwrap();
+
+  let (n, buf) = rt
+    .spawn(async move {
+      let stream = MANAGER.get_mut_stream(id);
+      if stream.is_none() {
+        return Err(SwiftKcpError::NoStreamForId { id });
+      }
+      let mut stream = stream.unwrap();
+      let mut buf: Vec<u8> = vec![0; READ_BUF];
+      let n = stream.read(&mut buf).await?;
+      Ok((n, buf))
+    })
+    .await??;
+
+  Ok(buf[0..n].to_vec())
 }
